6818ffbf3568ee9bddc5d4e7c539e1abc2b61321
rohan.gupta <rohan.gupta@zopper.com>
2022-01-22 22:20:08 +0530

    added codesplitting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import _ from "lodash";
 import "./App.css";
- 
-const App = ({name}) => {
-    return `<h3>Hi my name is ${name}</h3>`
-}
+
+const App = ({ name }) => {
+	return `
+    <h2> ${_.join(["Welcome,", name], " ")} </h2>
+    <p> Build apps with plain JS but with modern tooling  </p>
+    `;
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 
   module: {
@@ -30,7 +32,27 @@ const webpackConfig = {
     ],
   },
 
-  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: "Vanilla JS app",
+        template: path.resolve(__dirname, "src", "index.html"),
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+        cacheGroups: {
+            node_vendors: {
+                name: "vendor",
+                test: /[\\/]node_modules[\\/]/,
+                chunks: "all",
+                priority: 1
+            }
+        }
+    }
+},
+
+  mode: "production",
 };
 
 module.exports = webpackConfig;
